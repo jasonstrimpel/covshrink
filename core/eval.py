6778760398b58b67d165c7d1013fd2ef71babdf4
strimp099-win <strimp101@gmail.com>
2011-11-02 15:48:58 -0500

    further enhanced analysis, added plots

--- core/eval.py
+++ core/eval.py
@@ -5,8 +5,11 @@ from dateutil import relativedelta
 import math
 import time
 
+# application specific modules
+import pylab
+
 # custom modules
-import params2
+import params
 import portfolio
 import optimize as op
 import numpy as np
@@ -25,7 +28,7 @@ def eval(type, index=30):
     
     """
     # get the portfolio parameters
-    port_params = params2.get_portfolio_params(index=index)
+    port_params = params.get_portfolio_params(index=index)
 
     # instantiate the porfolio object
     port = portfolio.Portfolio(port_params, proxy={"http": "http://proxy.jpmchase.net:8443"})
@@ -49,10 +52,12 @@ def eval(type, index=30):
     # constant benchmark weights
     #returns = port.get_portfolio_historic_returns()
     active = port.get_active_returns()
+    bench_returns = port.get_benchmark_returns()
+    bench_weights = port.get_benchmark_weights()
 
     expected_excess_returns = port.get_expected_excess_stock_returns()
 
-    e = []
+    e = []; te = [];
 
     for i in xrange(roll, periods+roll+1):
         
@@ -90,26 +95,37 @@ def eval(type, index=30):
         # code trying to become comfortable with the package to no avail
         x = op.optimize(a, S)
         
-        # optimized expected excess portfolio returns
+        # optimized expected active portfolio returns
         e_ = (x.T * y).sum()
-
         e.append(e_)
+        
+        # weighted benchmark returns
+        b = (bench_returns.ix[end:end] * bench_weights.ix[end:end]).sum()
+        
+        # tracking error
+        te.append(e_ - b)
 
     return {
         'information_ratio': port.information_ratio(np.array([e])),
         'mean_excess_return': np.array([e]).mean(),
-        'stdev_excess_return': np.array([e]).std()
+        'stdev_excess_return': np.array([e]).std(),
+        'tracking_error': np.array([te]).std()
     }
 
 runs = 10
 N = [15, 30, 50, 75, 100]
-
+cnt = 0
 start = time.time()
+
+# lists for plots
+p_ir_sa = []; p_mer_sa = []; p_msd_sa = []; p_te_sa = [];
+p_ir_sh = []; p_mer_sh = []; p_msd_sh = []; p_te_sh = [];
 
 for n in N:
 
-    ir_sh = []; me_sh = []; se_sh = []; 
-    ir_sa = []; me_sa = []; se_sa = []; 
+    ir_sh = []; me_sh = []; se_sh = []; te_sh = [];
+    ir_sa = []; me_sa = []; se_sa = []; te_sa = [];
+        
     s = time.time()
     
     for i in xrange(runs):
@@ -117,29 +133,43 @@ for n in N:
         ir_sa.append(res['information_ratio'])
         me_sa.append(res['mean_excess_return'])
         se_sa.append(res['stdev_excess_return'])
+        te_sh.append(res['tracking_error'])
         
         res = eval('shrunk', index=n)
         ir_sh.append(res['information_ratio'])
         me_sh.append(res['mean_excess_return'])
         se_sh.append(res['stdev_excess_return'])
+        te_sa.append(res['tracking_error'])
         
-        #print 'run #',i+1,'of',runs,'(',round(((i+1.0)/runs),2)*100.0,'% complete)'
+        print 'run #',i+1,'of',runs,'(',round(((i+1.0)/runs),2)*100.0,'% complete)'
     
     # sample cov matrix stats
-    ir_sa = sum(ir_sa) / runs
-    mer_sa = sum(me_sa) / runs
-    msd_sa = sum(se_sa) / runs
+    p_ir_sa.append(sum(ir_sa) / runs)
+    p_mer_sa.append(sum(me_sa) / runs)
+    p_msd_sa.append(sum(se_sa) / runs)
+    p_te_sa.append(sum(te_sa) / runs)
 
     # shrunk cov matrix stats
-    ir_sh = sum(ir_sh) / runs
-    mer_sh = sum(me_sh) / runs
-    msd_sh = sum(se_sh) / runs
+    p_ir_sh.append(sum(ir_sh) / runs)
+    p_mer_sh.append(sum(me_sh) / runs)
+    p_msd_sh.append(sum(se_sh) / runs)
+    p_te_sh.append(sum(te_sh) / runs)
     
-    print '\t\tIR\t\tMean\tSD'
-    print 'Sample\t%.4f\t%.4f\t%.4f\t' % (ir_sa, mer_sa, msd_sa)
-    print 'Shrink\t%.4f\t%.4f\t%.4f\t' % (ir_sh, mer_sh, msd_sh)
+    print '\tIR\tMean\tSD\tTE'
+    print 'Sample\t%.4f\t%.4f\t%.4f\t%.4f\t' % (p_ir_sa[cnt], p_mer_sa[cnt], p_msd_sa[cnt], p_te_sa[cnt])
+    print 'Shrink\t%.4f\t%.4f\t%.4f\t%.4f\t' % (p_ir_sh[cnt], p_mer_sh[cnt], p_msd_sh[cnt], p_te_sh[cnt])
     print 'computed in', round(time.time()-s, 2), 'seconds'
     print 'N=%.0f\tRuns=%.0f' % (n, runs)
     print
 
+    cnt += 1
+
 print 'total run', round((time.time()-start)/60.0, 2), 'minutes'
+
+pylab.plot(N, p_ir_sa, 'r-', N, p_ir_sh, 'b-')
+pylab.xlabel('Index size, N')
+pylab.ylabel('Information Ratio, IR')
+pylab.title('Information Ratio v. Index Size')
+pylab.grid(True)
+pylab.legend(('Sample', 'Shrunk'))
+pylab.show()
