ef4a64bca1d94c7fc48df70c35e280763bfa21d1
strimp099-win <strimp101@gmail.com>
2011-11-02 15:49:50 -0500

    updated test module

--- core/test.py
+++ core/test.py
@@ -6,30 +6,28 @@ from itertools import islice
 import time
 
 # custom modules
-import params2
+import params
 import portfolio
 import optimize as op
 import numpy as np
 import pandas
 from cvxopt import matrix
 
-'''
-def jump_by_month(start_date, end_date, month_step=1): 
-    current_date = start_date 
-    while current_date < end_date: 
-        yield current_date 
-        carry, new_month = divmod(current_date.month - 1 + month_step, 12)
-        new_month += 1 
-        current_date = current_date.replace(year=current_date.year + carry, month=new_month) 
-'''
 
 # get the portfolio parameters
-port_params = params2.get_portfolio_params(index=125)
+port_params = params.get_portfolio_params(index=10)
 # instantiate the porfolio object
 port = portfolio.Portfolio(port_params, proxy={"http": "http://proxy.jpmchase.net:8443"})
 
+dates = port.get_trading_dates()
 
-print port.get_portfolio_historic_position_values().to_string()
+start = datetime(1998,1,1)
+end = datetime(1998,12,1)
+
+portvalue = port.get_portfolio_historic_position_values()
+
+
+#print port.get_portfolio_historic_position_values().to_string()
 
 '''
 bench = port.get_benchmark_weights().ix[datetime(1990,02,01)]
@@ -47,4 +45,14 @@ active = port.get_active_returns()
 print 
 print 
 print active.ix[datetime(1990,03,01)]
+'''
+
+'''
+def jump_by_month(start_date, end_date, month_step=1): 
+    current_date = start_date 
+    while current_date < end_date: 
+        yield current_date 
+        carry, new_month = divmod(current_date.month - 1 + month_step, 12)
+        new_month += 1 
+        current_date = current_date.replace(year=current_date.year + carry, month=new_month) 
 '''
