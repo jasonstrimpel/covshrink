17c88b4abf60ba61b7428fb88500568dbba673a1
strimp099-win <strimp101@gmail.com>
2011-11-03 10:45:07 -0500

    added comments

--- core/eval.py
+++ core/eval.py
@@ -17,15 +17,17 @@ import pandas
 from cvxopt import matrix
 
 def eval(type, index=30):
-    """
+    """Executes the experiment
     
     Parameters
     ----------
+    type : a string representing the type of covariance matrix to optimize against, either 'sample' or 'shrunk'
+    index : benchmark index size to use
     
     Returns
     -------
-    
-    
+    dictionary : returns a dictionary with sample statistics for the information ratio, mean excess return,
+        standard deviation of excess returns, and tracking error
     """
     # get the portfolio parameters
     port_params = params.get_portfolio_params(index=index)
@@ -112,64 +114,71 @@ def eval(type, index=30):
         'tracking_error': np.array([te]).std()
     }
 
-runs = 10
-N = [15, 30, 50, 75, 100]
-cnt = 0
-start = time.time()
-
-# lists for plots
-p_ir_sa = []; p_mer_sa = []; p_msd_sa = []; p_te_sa = [];
-p_ir_sh = []; p_mer_sh = []; p_msd_sh = []; p_te_sh = [];
-
-for n in N:
-
-    ir_sh = []; me_sh = []; se_sh = []; te_sh = [];
-    ir_sa = []; me_sa = []; se_sa = []; te_sa = [];
-        
-    s = time.time()
+def run(runs=10, index=[15, 30, 50, 75, 100]):
+    """Interface for the execution script
     
-    for i in xrange(runs):
-        res = eval('sample', index=n)
-        ir_sa.append(res['information_ratio'])
-        me_sa.append(res['mean_excess_return'])
-        se_sa.append(res['stdev_excess_return'])
-        te_sh.append(res['tracking_error'])
+    Parameters
+    ----------
+    runs : number of runs to use to calculate the mean sample statistic
+    index : list of benchmark index sizes to use
+
+    """
+    cnt = 0
+    start = time.time()
+
+    # lists for plots
+    p_ir_sa = []; p_mer_sa = []; p_msd_sa = []; p_te_sa = [];
+    p_ir_sh = []; p_mer_sh = []; p_msd_sh = []; p_te_sh = [];
+
+    for n in index:
+
+        ir_sh = []; me_sh = []; se_sh = []; te_sh = [];
+        ir_sa = []; me_sa = []; se_sa = []; te_sa = [];
+            
+        s = time.time()
         
-        res = eval('shrunk', index=n)
-        ir_sh.append(res['information_ratio'])
-        me_sh.append(res['mean_excess_return'])
-        se_sh.append(res['stdev_excess_return'])
-        te_sa.append(res['tracking_error'])
+        for i in xrange(runs):
+            res = eval('sample', index=n)
+            ir_sa.append(res['information_ratio'])
+            me_sa.append(res['mean_excess_return'])
+            se_sa.append(res['stdev_excess_return'])
+            te_sh.append(res['tracking_error'])
+            
+            res = eval('shrunk', index=n)
+            ir_sh.append(res['information_ratio'])
+            me_sh.append(res['mean_excess_return'])
+            se_sh.append(res['stdev_excess_return'])
+            te_sa.append(res['tracking_error'])
+            
+            print 'run #',i+1,'of',runs,'(',round(((i+1.0)/runs),2)*100.0,'% complete)'
         
-        print 'run #',i+1,'of',runs,'(',round(((i+1.0)/runs),2)*100.0,'% complete)'
-    
-    # sample cov matrix stats
-    p_ir_sa.append(sum(ir_sa) / runs)
-    p_mer_sa.append(sum(me_sa) / runs)
-    p_msd_sa.append(sum(se_sa) / runs)
-    p_te_sa.append(sum(te_sa) / runs)
+        # sample cov matrix stats
+        p_ir_sa.append(sum(ir_sa) / runs)
+        p_mer_sa.append(sum(me_sa) / runs)
+        p_msd_sa.append(sum(se_sa) / runs)
+        p_te_sa.append(sum(te_sa) / runs)
 
-    # shrunk cov matrix stats
-    p_ir_sh.append(sum(ir_sh) / runs)
-    p_mer_sh.append(sum(me_sh) / runs)
-    p_msd_sh.append(sum(se_sh) / runs)
-    p_te_sh.append(sum(te_sh) / runs)
-    
-    print '\tIR\tMean\tSD\tTE'
-    print 'Sample\t%.4f\t%.4f\t%.4f\t%.4f\t' % (p_ir_sa[cnt], p_mer_sa[cnt], p_msd_sa[cnt], p_te_sa[cnt])
-    print 'Shrink\t%.4f\t%.4f\t%.4f\t%.4f\t' % (p_ir_sh[cnt], p_mer_sh[cnt], p_msd_sh[cnt], p_te_sh[cnt])
-    print 'computed in', round(time.time()-s, 2), 'seconds'
-    print 'N=%.0f\tRuns=%.0f' % (n, runs)
-    print
+        # shrunk cov matrix stats
+        p_ir_sh.append(sum(ir_sh) / runs)
+        p_mer_sh.append(sum(me_sh) / runs)
+        p_msd_sh.append(sum(se_sh) / runs)
+        p_te_sh.append(sum(te_sh) / runs)
+        
+        print '\tIR\tMean\tSD\tTE'
+        print 'Sample\t%.4f\t%.4f\t%.4f\t%.4f\t' % (p_ir_sa[cnt], p_mer_sa[cnt], p_msd_sa[cnt], p_te_sa[cnt])
+        print 'Shrink\t%.4f\t%.4f\t%.4f\t%.4f\t' % (p_ir_sh[cnt], p_mer_sh[cnt], p_msd_sh[cnt], p_te_sh[cnt])
+        print 'computed in', round(time.time()-s, 2), 'seconds'
+        print 'N=%.0f\tRuns=%.0f' % (n, runs)
+        print
 
-    cnt += 1
+        cnt += 1
 
-print 'total run', round((time.time()-start)/60.0, 2), 'minutes'
+    print 'total run', round((time.time()-start)/60.0, 2), 'minutes'
 
-pylab.plot(N, p_ir_sa, 'r-', N, p_ir_sh, 'b-')
-pylab.xlabel('Index size, N')
-pylab.ylabel('Information Ratio, IR')
-pylab.title('Information Ratio v. Index Size')
-pylab.grid(True)
-pylab.legend(('Sample', 'Shrunk'))
-pylab.show()
+    pylab.plot(N, p_ir_sa, 'r-', N, p_ir_sh, 'b-')
+    pylab.xlabel('Index size, N')
+    pylab.ylabel('Information Ratio, IR')
+    pylab.title('Information Ratio v. Index Size')
+    pylab.grid(True)
+    pylab.legend(('Sample', 'Shrunk'))
+    pylab.show()
